8943b6064cdcd89f214bab24f02eb4b2a2897d6f
patr543a <113888937+patr543a@users.noreply.github.com>
2023-01-25 14:31:40 +0100

    Dansk

--- Others/Program.cs
+++ Others/Program.cs
@@ -2,12 +2,12 @@
 
 var r = Reg();
 
-var m = r.Match("AOZ-270");
+var m = r.Match("AOÅ-270");
 
 Console.WriteLine($"Matched? {m.Success}");
 
 partial class Program
 {
-    [GeneratedRegex("^[A-Z]{3}\\-[1-9][0-9]{2}$")]
+    [GeneratedRegex("^[A-Å]{3}\\-[1-9][0-9]{2}$")]
     private static partial Regex Reg();
 }
